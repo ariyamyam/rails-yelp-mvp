23d761ccae7f24934ca053669a1144bc0f793c70
Sophie Hadeln <sophiehadeln@gmail.com>
2022-02-16 16:42:34 +0100

    ugly but working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  # user can see all restos
+  get "restaurants", to: "restaurants#index"
+
+  # user can add a new resto
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+
+  # and be redirected to the show view of that new restaurant. (resto has no reviews yet.)
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  get "restaurants/:restaurants_id/reviews", to: "reviews#index"
+
+  # A visitor can add a new review to a restaurant
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: :new_restaurant_review
+  post "restaurants/:restaurant_id/reviews", to: "reviews#create", as: :restaurant_reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+goldmarie = { name: "Goldmarie", address: "Schmellerstrasse, Munich", phone_number: "123 2233", category: "french" }
+anna = { name: "Anna", address: "Daiserstrasse, Munich", phone_number: "123 4455", category: "italian" }
+pommes = { name: "Bruxelles", address: "Place du Gourmet, Gent", phone_number: "123 2233", category: "belgian" }
+peking = { name: "Peking", address: "Rothembaum, Hamburg", phone_number: "123 2233", category: "chinese" }
+suzuki = { name: "Best Sushi", address: "Pohlstrasse, Berlin", phone_number: "123 2233", category: "japanese" }
+
+[goldmarie, anna, pommes, peking, suzuki].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
